df0a8c5f8b4da81236e7dd93fb6084f290ef0bc0
nmqit55 <nmqit55@gmail.com>
2023-10-04 15:17:19 +0700

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -6,12 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
+
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
+app.UseCors(builder => builder
+    .AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
@@ -21,3 +25,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+//app.UseCors();
