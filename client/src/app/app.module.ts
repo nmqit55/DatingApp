b4f9bbbf6c56797156fa1d38f9ac4639f81f6951
nmqit55 <nmqit55@gmail.com>
2023-10-19 16:27:54 +0700

    End of section 10.7

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './_services/members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptor/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
