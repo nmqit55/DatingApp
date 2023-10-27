0da9325678711d122cbdd945409326876dd7d904
nmqit55 <nmqit55@gmail.com>
2023-10-27 14:11:30 +0700

    End of section 13.4

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,7 @@ import { MembersService } from './../../_services/members.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_modules/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -9,11 +10,27 @@ import { Member } from 'src/app/_models/member';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]> | undefined;
+  // members$: Observable<Member[]> | undefined;
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
-    this.members$ = this.memberService.getMembers();
+    // this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: response =>{
+        if(response.result && response.pagination){
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      }
+    })
   }
 }
