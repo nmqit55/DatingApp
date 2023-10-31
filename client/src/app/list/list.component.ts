2dabeb79453af40fe9729b0a6d179e507d9cfad5
nmqit55 <nmqit55@gmail.com>
2023-10-31 16:43:12 +0700

    End of section 14.7

--- client/src/app/list/list.component.ts
+++ client/src/app/list/list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+
+  members: Member[] | undefined;
+  predicate = 'liked'
+
+  constructor(private memberService: MembersService) {
+
+  }
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe({
+      next: response => {
+        this.members = response
+      }
+    })
+  }
 
 }
